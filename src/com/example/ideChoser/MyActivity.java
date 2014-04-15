f350e2979ebcbfa57e45d899c860845136e73c54
mejatysek <mejatysek@gmail.com>
2014-04-15 23:32:13 +0200

    Only git changes

--- src/com/example/ideChoser/MyActivity.java
+++ src/com/example/ideChoser/MyActivity.java
@@ -15,14 +15,16 @@ public class MyActivity extends Activity {
         setContentView(R.layout.linear);
         //getActionBar().hide();
     }
-    public void clickFirst(View view){
 
+    public void clickFirst(View view) {
         Toast.makeText(this, "Click on first button", Toast.LENGTH_LONG).show();
     }
-    public void clickSecond(View view){
+
+    public void clickSecond(View view) {
         Toast.makeText(this, "Click on second button", Toast.LENGTH_LONG).show();
     }
-    public void clickThird(View view){
+
+    public void clickThird(View view) {
         Toast.makeText(this, "Click on third button", Toast.LENGTH_LONG).show();
     }
 }
