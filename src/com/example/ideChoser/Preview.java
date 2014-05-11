6b10246f31f06cc9457b0cc5236afe2865857447
mejatysek <mejatysek@gmail.com>
2014-05-11 11:15:28 +0200

    pridan ping to zoom do nahledu

--- src/com/example/ideChoser/Preview.java
+++ src/com/example/ideChoser/Preview.java
@@ -2,18 +2,58 @@ package com.example.ideChoser;
 
 import android.app.Activity;
 import android.graphics.BitmapFactory;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import com.imagezoom.ImageAttacher;
+import com.imagezoom.ImageAttacher.OnMatrixChangedListener;
+import com.imagezoom.ImageAttacher.OnPhotoTapListener;
 
 /**
  * Created by mejty on 11.5.14.
  */
+
 public class Preview extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+    ImageView nahled;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preview);
-        ImageView nahled = (ImageView) findViewById(R.id.previewImg);
-        //nahled.setBackgroundDrawable(BitmapDrawable.createFromPath(StaticData.IMAGE_FILE.getPath()));
+        nahled = (ImageView) findViewById(R.id.previewImg);
+
         nahled.setImageBitmap(BitmapFactory.decodeFile(StaticData.IMAGE_FILE.getPath()));
+
+        /**
+         * Use Simple ImageView
+         */
+        usingSimpleImage(nahled);
+
+    }
+
+    public void usingSimpleImage(ImageView imageView) {
+        ImageAttacher mAttacher = new ImageAttacher(imageView);
+        ImageAttacher.MAX_ZOOM = 2.0f; // Double the current Size
+        ImageAttacher.MIN_ZOOM = 0.5f; // Half the current Size
+        MatrixChangeListener mMaListener = new MatrixChangeListener();
+        mAttacher.setOnMatrixChangeListener(mMaListener);
+        PhotoTapListener mPhotoTap = new PhotoTapListener();
+        mAttacher.setOnPhotoTapListener(mPhotoTap);
+    }
+
+    private class PhotoTapListener implements OnPhotoTapListener {
+
+        @Override
+        public void onPhotoTap(View view, float x, float y) {
+        }
+    }
+
+    private class MatrixChangeListener implements OnMatrixChangedListener {
+
+        @Override
+        public void onMatrixChanged(RectF rect) {
+
+        }
     }
 }
