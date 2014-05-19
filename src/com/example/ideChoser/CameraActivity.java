4f6c2f499b5526632eb52a35cba0918f269aea52
Tomas Skalicky <skalito1@fel.cvut.cz>
2014-05-19 10:30:07 +0200

    Opraveno natočení kamery

--- src/com/example/ideChoser/CameraActivity.java
+++ src/com/example/ideChoser/CameraActivity.java
@@ -8,6 +8,7 @@ import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -156,6 +157,7 @@ public class CameraActivity extends Activity {
         super.onStart();
         // Create an instance of Camera
         mCamera = CameraSingleton.GetInstance();
+        this.setCameraDisplayOrientation(this, 1, mCamera);
     }
 
     @Override
@@ -174,6 +176,32 @@ public class CameraActivity extends Activity {
 // no camera on this device
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
+
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
 
 
 }
